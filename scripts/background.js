7ac780e17ea2c23638609be84b899a98771e6804
patrioticcow <cristi.catea@gmail.com>
2014-03-25 15:48:01 -0700

    a1

--- scripts/background.js
+++ scripts/background.js
@@ -9,17 +9,15 @@ chrome.browserAction.setBadgeText({text: '\'Allo'});
 console.log('\'Allo \'Allo! Event Page for Browser Action');
 
 
-
-
-console.log(chrome.windows.getCurrent({}, function(callback){
+console.log(chrome.windows.getCurrent({}, function (callback) {
     console.log(callback);
 }));
 
-chrome.windows.getCurrent(function(win)
-{
-    chrome.tabs.getAllInWindow(win.id, function(tabs)
-    {
+chrome.windows.getCurrent(function (win) {
+    chrome.tabs.getAllInWindow(win.id, function (tabs) {
         // Should output an array of tab objects to your dev console.
         console.debug(tabs);
     });
 });
+
+
--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -2,3 +2,4 @@
 
 console.log('\'Allo \'Allo! Content script');
 
+$('body').after('<div id="split_view">ccccccccc</div>');
