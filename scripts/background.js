45c83e936eaa7adf69b59f2cb8ba184acfb42a35
patrioticcow <cristi.catea@gmail.com>
2014-03-25 13:46:18 -0700

    initial commit

---
+++ scripts/background.js
@@ -0,0 +1,25 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    //console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({text: '\'Allo'});
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+
+
+
+console.log(chrome.windows.getCurrent({}, function(callback){
+    console.log(callback);
+}));
+
+chrome.windows.getCurrent(function(win)
+{
+    chrome.tabs.getAllInWindow(win.id, function(tabs)
+    {
+        // Should output an array of tab objects to your dev console.
+        console.debug(tabs);
+    });
+});
