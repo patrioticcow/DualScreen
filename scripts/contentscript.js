51e2425cb90c46044e273ca61a9f60e6187700aa
patrioticcow <cristi.catea@gmail.com>
2014-03-25 17:48:20 -0700

    a2

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -2,4 +2,10 @@
 
 console.log('\'Allo \'Allo! Content script');
 
-$('body').after('<div id="split_view">ccccccccc</div>');
+if (document.URL === '') {
+    $('body').after('<div id="split_view" style="position: fixed;width: 100%;bottom: 0;background: #FFF;z-index: 1000;"></div>');
+
+    $("#split_view").load(chrome.extension.getURL("panel.html"), function () {
+
+    });
+}
